62dc889a9680b96bf39192a2b820f3dbef1ff70d
sowmyav27 <viswanathan.sowmya@gmail.com>
2017-09-25 00:58:34 +0100

    First commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+Votechain 
+Voting is a fundamental right of every citizen of a democratic country. This is done to choose their government, who work for the betterment of the country. But we come across many instances of frauds happening within the system, from ballot stuffing and voter impersonation, to illegal activities of the electoral officers. Also, there are various ways of casting a vote, be it EVMs or paper ballots, and securing all these methods is challenging. In this paper, we propose a uniform online voting system called VoteChain, using blockchain technology as the security provider. Using this system, the voting process can be secured against vote tampering, as we record all the transactions made. 
+
+Problem Statement: 
+
+Today’s elections can be manipulated and voters can be coerced to vote against their will. Many a times, the credibility of the election process itself is questionable. In such a scenario, we need an election process that is transparent, fair, inexpensive and convenient. No singular protocol for managing votes. Voting equipment varies across jurisdiction. Several different voting systems are commonly employed, including paper ballots, Directly Recording Electronic (DRE) systems and Electronically operated vote counting machines (EVMs). Punch card voting systems are also used, as well as ballot marking devices to assist disabled voters. In addition to votes being cast in a multitude of way, that information is also recorded and stored non-uniformly. Paper ballots are typically scanned into a computer system, either at the polling place or a centralized location for that jurisdiction. DRE systems store vote information in the machine’s hard drive and some systems have the capacity to generate a paper audit trail, but not all jurisdictions maintain an audit trail
+
+During the process of voting, various tampering happening in between from the time the vote is casted till it is counted. Since there are many methodologies of voting process, the below listed tampering happens.
+
+Proposed Solution:
+
+Blockchain enables secure method of casting votes. Hyperledger records all the transactions, ensuring fraud detection. Web server facilitates access from remote systems.
+
+  The solution explores the current security shortcomings of the voting system, as well as the feasibility of an online voting system implemented using blockchain technology. Despite security risks and concerns, block-chain data storage in combination with electronic voting is a progressive and secure option for our voting systems. Blockchain technology makes it possible to attain a highly credible and verifiable election process at an inexpensive cost. 
+
+Flow of the process:
+
+We have two transfers during this process, one from the creation of template to the polling station. The vote is being casted by the citizen at this place once its been transferred from initial stage and from the polling station we have the second transfer to the counting station. Any changes made in between the transfers will be recorded in the Hyperledger and it can be tracked easily. 
---
+++ Server_Side/blockchain/participants/participants_info.js
@@ -0,0 +1,159 @@
+'use strict';
+
+let participants_info = {
+    'regulators': [
+        {
+            'name': 'PO',
+            'identity': 'PO',
+            'password': 'IUZCYDngtwjW',
+            'address_line_1': 'St Andrew Presbyterian Church',
+            'address_line_2': '47192 Monroe St',
+            // 'address_line_3': 'Indio',
+            'address_line_3': 'Indio, CA',
+            'postcode': '92201-6740'
+        }
+    ],
+    'manufacturers': [
+        {
+            'name': 'Station 1',
+            'identity': 'Station 1',
+            'password': 'fGMMQqWEPxVy',
+            'address_line_1': 'Desert Grove Clubhouse',
+            'address_line_2': '81641 Avenue Forty Eighth',
+            'address_line_3': 'San Jose, CA',
+            'postcode': '95112'
+        },
+        {
+            'name': 'Station 2',
+            'identity': 'Station 2',
+            'password': 'hTpPFxiOwWgS',
+            'address_line_1': 'Andrew Jackson Elementary School',
+            'address_line_2': '82850 Kenner Ave',
+            'address_line_3': 'Sonoma, IL',
+            'postcode': '92341'
+        },
+        {
+            'name': 'Station 3',
+            'identity': 'Station 3',
+            'password': 'nNRyjPKrSpUb',
+            'address_line_1': 'Heritage Palms Master H O A',
+            'address_line_2': '44291 S Heritage Palms Dr',
+            'address_line_3': 'Sunnyvale, TX',
+            'postcode': '98736'
+        }
+    ],
+    'dealerships': [
+        {
+            'name': 'Counting Station',
+            'identity': 'Counting Station',
+            'password': 'TvNWKDgWTrfH',
+            'address_line_1': '84 Hull Road',
+            'address_line_2': 'Dublin, CA',
+            'postcode': '98465'
+        },
+        // {
+        //     'name': 'Milescape',
+        //     'identity': 'Milescape',
+        //     'password': 'cGJslZqjNjPK',
+        //     'address_line_1': 'Imperial Yard',
+        //     'address_line_2': 'Derby',
+        //     'postcode': 'DE94 8HY'
+        // },
+        // {
+        //     'name': 'Viewers Alfa Romeo',
+        //     'identity': 'Viewers_Alfa_Romeo',
+        //     'password': 'VWjFucyEIzTn',
+        //     'address_line_1': '25 Lower Lens Street',
+        //     'address_line_2': 'Penylan',
+        //     'address_line_3': 'Cardiff',
+        //     'address_line_4': 'South Glamorgan',
+        //     'postcode': 'CF28 9LC'
+        // }
+    ],
+    'lease_companies': [
+        {
+            'name': 'LeaseCan',
+            'identity': 'LeaseCan',
+            'password': 'mRbbQTpZfVVa',
+            'address_line_1': '64 Zoo Lane',
+            'address_line_2': 'Slough',
+            'address_line_3': 'Berkshire',
+            'postcode': 'SL82 4AB'
+        },
+        {
+            'name': 'Every Car Leasing',
+            'identity': 'Every_Car_Leasing',
+            'password': 'KakjewJfwBSq',
+            'address_line_1': '9 Main Road',
+            'address_line_2': 'Mobberly',
+            'address_line_3': 'Cheshire',
+            'postcode': 'WA18 7KJ'
+        },
+        {
+            'name': 'Regionwide Vehicle Contracts',
+            'identity': 'Regionwide_Vehicle_Contracts',
+            'password': 'plqOUyoFTZyK',
+            'address_line_1': 'Unit 9',
+            'address_line_2': 'Malcom Christie Way',
+            'address_line_3': 'Riggot Fields',
+            'address_line_4': 'Manchester',
+            'postcode': 'M21 15QY'
+        }
+    ],
+    'leasees': [
+        {
+            'name': 'Joe Payne',
+            'identity': 'Joe_Payne',
+            'password': 'BKwnxTfJGNyK',
+            'address_line_1': '84 Byron Road',
+            'address_line_2': 'Eastleigh',
+            'postcode': 'SO50 8JR'
+        },
+        {
+            'name': 'Andrew Hurt',
+            'identity': 'Andrew_Hurt',
+            'password': 'tkGIRxBywwMk',
+            'address_line_1': '16 West Street',
+            'address_line_2': 'Eastleigh',
+            'postcode': 'SO50 9CL'
+        },
+        {
+            'name': 'Anthony O\'Dowd',
+            'identity': 'Anthony_O_Dowd',
+            'password': 'MrcAgjpBwhmI',
+            'address_line_1': '34 Main Road',
+            'address_line_2': 'Winchester',
+            'postcode': 'SO50 3QV'
+        }
+    ],
+    'scrap_merchants': [
+        {
+            'name': 'Cray Bros (London) Ltd',
+            'identity': 'Cray_Bros_London_Ltd',
+            'password': 'BTaWHtHrCZry',
+            'address_line_1': '26 Electric Eel Avenue',
+            'address_line_2': 'Twickenham',
+            'address_line_3': 'Greater London',
+            'postcode': 'SE51 9DR'
+        },
+        {
+            'name': 'Aston Scrap Centre',
+            'identity': 'Aston_Scrap_Centre',
+            'password': 'AzdeAZuGtlUT',
+            'address_line_1': '11 Willow Park Way',
+            'address_line_2': 'Aston on Trent',
+            'address_line_3': 'Derby',
+            'postcode': 'DE72 2DG'
+        },
+        {
+            'name': 'ScrapIt! UK',
+            'identity': 'ScrapIt_UK',
+            'password': 'WDYJcenyScyC',
+            'address_line_1': '25 Lincoln Road',
+            'address_line_2': 'Winchester',
+            'postcode': 'SO29 9BL'
+        }
+    ]
+};
+
+module.exports = participants_info;
